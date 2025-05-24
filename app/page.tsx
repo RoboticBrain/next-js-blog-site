7064536ba5ac8c2b769df6db541a013448367681
RoboticBrain <mazharahmed0118@gmail.com>
2025-05-24 15:57:26 +0500

    removed suspense components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-
-// import { prisma, PrismaClient } from "";
-
-// import { Prisma } from "@prisma/client";
-import { defaultOverrides } from "next/dist/server/require-hook";
 import BlogpostCard from "./components/general/BlogpostCard";
 import prisma from "./utils/db";
 import { Suspense } from "react";
@@ -29,26 +24,16 @@ async function getData() {
   return data;
 }
 
-export default function Home() {
-
-  return (
+export default async function Home() {
+    const data = await getData();
+    return (
     <>
     <div className="py-6">
       <h1 className="text-3xl font-bold tracking-tight mb-8">Latest Posts</h1>
-      <Suspense fallback={<p>loading content...</p>}>
-        <BlogPosts/>
-      </Suspense>
-    
+         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map(item => <BlogpostCard data={item} key={item.id}/> )}
+      </div>
     </div>
    </>
   );
-}
-export  async function BlogPosts() {
-   const data = await getData();
-   return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map(item => <BlogpostCard data={item} key={item.id}/> )}
-
-    </div>
-   )
 }
