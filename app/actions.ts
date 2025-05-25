db838ce37ba6523d1c8d3dc11ff3a8a04f0a3bb1
RoboticBrain <mazharahmed0118@gmail.com>
2025-05-25 16:19:11 +0500

    cached

--- app/actions.ts
+++ app/actions.ts
@@ -2,6 +2,7 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import prisma from './utils/db';
 import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
 export async function handleSubmission(formData: FormData) {
      const {getUser} = getKindeServerSession();
     const user = await getUser();
@@ -22,5 +23,6 @@ export async function handleSubmission(formData: FormData) {
         }
    
     )
+         revalidatePath("/");
          redirect('/dashboard');
 }
